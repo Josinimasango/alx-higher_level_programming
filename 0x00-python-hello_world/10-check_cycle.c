dcf5ac7a507c2a7dc7d4f274ae7c5116b19560ed
Josinimasango <nellyjossydumako@.com>
2023-09-06 11:47:03 +0000

    Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,8 +2,10 @@
 
 /**
  * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
  *
- * Return: 0 if there is no cycle, 1 if there is a cycle
+ * Return: 1 if there is cycle, 0 if there is no cycle
+ */
 int check_cycle(listint_t *list)
 {
 	listint_t *slow = list;
