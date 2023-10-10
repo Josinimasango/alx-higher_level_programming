0462ca412719d34343667008112d5065c6e1ae78
Josinimasango <nellyjossydumako@.com>
2023-10-10 12:50:53 +0000

    base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines the base geometry of class BaseGeometry."""
+
+
+class BaseGeometry:
+    """represent the base geometry."""
+
+    def area(self):
+        """not implemented."""
+        raise Exception("area() is not implemented")
