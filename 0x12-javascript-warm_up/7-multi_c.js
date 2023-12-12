ad2cdbd36ae98227735b846095fcc1dc12cf2dfb
Josini masango <nellyjossydumako@gmail.com>
2023-12-12 03:36:24 -0800

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
-if (process.argv.length < 3) {
-   console.log('missing number of occurances');
-} else  {
-	const x = parseInt(process.argv[2]);
-
-	if (isNaN(x)) {
-	   console.log('missing number of occurances');
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
 } else {
-   let i = 0;
+  const x = Number(process.argv[2]);
+  let i = 0;
   while (i < x) {
-     console.log('C is fun');
-     i++;
-    } 
+    console.log('C is fun');
+    i++;
   }
 }
