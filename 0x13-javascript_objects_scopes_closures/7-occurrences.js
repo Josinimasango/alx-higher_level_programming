0e2c60d67f027c48ab2cbb2340e1ce2dd552d7d6
Josini masango <nellyjossydumako@gmail.com>
2023-12-15 05:57:35 -0800

    occurences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-    let nbOccurences = 0;
+    let nOccurences = 0;
 
     for (let i = 0; i < list.length; i++) {
         if (list[i] === searchElement) {
-            nbOccurences++;
+            nOccurences++;
         }
     }
 
-    return nbOccurences;
+    return nOccurences;
 }
