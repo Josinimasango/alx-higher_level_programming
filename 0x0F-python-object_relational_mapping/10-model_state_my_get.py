39c9228a9434af69626720ae0e50be3ffdc30778
Josini masango <nellyjossydumako@gmail.com>
2023-12-28 11:23:46 -0800

    my get

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3
-"""Prints the State object with the name passed as argument from the database"""
+"""prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa"""
 
-import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import sys
 
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+    host = 'localhost'
+    port = '3306'
+
+    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
+                           username, password, host, port, db_name),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-    session = Session()
-    instance = session.query(State).filter(State.name == (sys.argv[4],))
-    try:
-        print(instance[0].id)
-    except IndexError:
-        print("Not found")
+    local_session = Session()
+    result = local_session.query(State).filter(
+                            State.name.like(state_name)
+                            ).first()
+    local_session.close()
+    engine.dispose()
+
+    if result:
+        print(result.id)
+    else:
+        print('Not found')
